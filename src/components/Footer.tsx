8edff135bf942551ed0c1d10d3762725be789bfa
Rejolin Solomon J <87459641+RejolinSolomonJ@users.noreply.github.com>
2025-05-13 10:11:58 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -72,7 +72,7 @@ const Footer = () => {
               <p className="mb-2">India</p>
               <p className="mb-2">Phone: +91 12345 67890</p>
               <p>Email: saiashwiniacademy@gmail.com</p>
-              <p>Email: kraman841@gmail.com</p>
+              <p>       kraman841@gmail.com</p>
             </address>
           </div>
         </div>
